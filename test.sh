d82164ae5e5491433c565847c79015270c2b07f4
Dvoraak <dorian.poncet@gmail.com>
2020-05-25 17:48:26 +0200

    [TESTSUITE] Tests for all four graphs (no reorder and bfs from root)

--- test.sh
+++ test.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 
-echo Testing with inet without reordering
-python ./src/main.py ./data/inet noreorder
-echo Testing with inet reordering from root zero
-python ./src/main.py ./data/inet zero
+
+echo Testing with inet, without reordering
+python3 ./src/main.py ./data/inet noreorder
+echo Testing with inet, reordering from root zero
+python3 ./src/main.py ./data/inet zero
+echo Testing with ip, without reordering
+python3 ./src/main.py ./data/ip noreorder
+echo Testing with ip, reordering from root zero
+python3 ./src/main.py ./data/ip zero
+echo Testing with p2p, without reordering
+python3 ./src/main.py ./data/p2p noreorder
+echo Testing with p2p, reordering from root zero
+python3 ./src/main.py ./data/p2p zero
+echo Testing with web, without reordering
+python3 ./src/main.py ./data/web noreorder
+echo Testing with web, reordering from root zero
+python3 ./src/main.py ./data/web zero
+
+
+#OTHER ROOT CHOICES
+
 #echo Testing with inet reordering from center 
 #python3 ./src/main.py ./data/inet center
 #echo Testing with inet reordering from root with mindegree
@@ -12,7 +29,3 @@ python ./src/main.py ./data/inet zero
 #python3 ./src/main.py ./data/inet maxdegree
 #echo Testing with inet reordering from extreme root found with doublesweep
 #python3 ./src/main.py ./data/inet doublesweep
-
-#echo Testing with ip TODO
-#echo Testing with p2p TODO
-#echo Testing with web TODO
