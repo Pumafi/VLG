9413d4244536ae9711f744d9649a34ec70ec39c9
Dvoraak <dorian.poncet@gmail.com>
2020-05-25 15:49:46 +0200

    [TESTSUITE] Removed test with mindegree/maxdegree, not implemented

--- test.sh
+++ test.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-echo Testing with inet at root zero
+echo Testing with inet without reordering
+python3 ./src/main.py ./data/inet noreorder
+echo Testing with inet reordering from root zero
 python3 ./src/main.py ./data/inet zero
-echo Testing with inet at root mindegree
-python3 ./src/main.py ./data/inet mindegree
-echo Testing with inet at root maxdegree
-python3 ./src/main.py ./data/inet maxdegree
-echo Testing with ip TODO
-echo Testing with p2p TODO
-echo Testing with web TODO
+#echo Testing with inet reordering from center 
+#python3 ./src/main.py ./data/inet center
+#echo Testing with inet reordering from root with mindegree
+#python3 ./src/main.py ./data/inet mindegree
+#echo Testing with inet reordering from root with maxdegree
+#python3 ./src/main.py ./data/inet maxdegree
+#echo Testing with inet reordering from extreme root found with doublesweep
+#python3 ./src/main.py ./data/inet doublesweep
+
+#echo Testing with ip TODO
+#echo Testing with p2p TODO
+#echo Testing with web TODO
