c5c2bba95b4350035f2bcb319490336ab38ea748
Dvoraak <dorian.poncet@gmail.com>
2020-05-25 15:46:36 +0200

    [INPUT] Can now choose whether reordering or not
    This way, no redundancy in tests

--- src/main.py
+++ src/main.py
@@ -90,11 +90,21 @@ def main():
     check_version_ig()
 
     # Read Graph
-    center = -1
-    if len(sys.argv) == 1: #no args, random graph
-        g = ig.Graph.GRG(1000, 0.1)
+    if len(sys.argv) < 3: #no args, random graph -> not anymore
+        #g = ig.Graph.GRG(1000, 0.1)
+        print("ERROR: need to specify graph and starting node for reordering.")
+        print("Choices are: \"noreorder\", \"zero\", \"center\", \"mindegree\", \"maxdegree\", \"doublesweep\".")
+        exit(1)
     else: #using provided graph file path
+
+        #get parameters
         filepath = sys.argv[1]
+        if sys.argv[2] in ["noreorder", "zero", "center", "mindegree", "maxdegree", "doublesweep"]:
+            root = sys.argv[2]
+        else:
+            print("Error: root", sys.argv[2], "not recognized.")
+            exit(1)
+
         try:
             with open(filepath) as f:
                 g = ig.Graph.Read_Edgelist(f, directed=False)
@@ -102,26 +112,22 @@ def main():
         except:
             print("Could not parse graph file \"" + filepath + "\"")
             exit(1)
-        try:
-            with open(filepath + '.center') as f:
-                center = int(f.readlines()[0])
-                print("Center:", center)
-        except Exception as e:
-            print(e)
-            print("No center file named \"" + filepath + ".center\" found.")
 
-    if len(sys.argv) > 2:
-        if sys.argv[2] in ["zero", "center", "mindegree", "maxdegree", "doublesweep"]:
-            root = sys.argv[2]
-        else:
-            print("Root", sys.argv[2], "not recognized. Using root \"zero\".")
-            root = "zero"
-    else:
-        root = "zero"
+        center = -1
+        if root == "center":
+            try:
+                with open(filepath + '.center') as f:
+                    center = int(f.readlines()[0])
+                    print("Center:", center)
+            except Exception as e:
+                print("No center file named \"" + filepath + ".center\" found.")
+                exit(1)
 
-    # Apply Leiden algorithm
-    clusters = g.community_leiden(objective_function="modularity")
-    print("Graph: modularity = {0}, number of clusters: {1}".format(clusters.modularity, len(clusters)))
+            # Apply Leiden algorithm
+    if root == "noreorder":
+        clusters = g.community_leiden(objective_function="modularity")
+        print("Graph: modularity = {0}, number of clusters: {1}".format(clusters.modularity, len(clusters)))
+        exit(0)
 
     if DRAW:
         draw_clustered_graph_fa2(g, clusters)
